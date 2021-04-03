2859518aa1f71e561c45ab34e3ec3179c095dfa4
mellonfarmer <shaunbu13@yahoo.co.uk>
2021-04-03 11:53:23 +0100

    Add files via upload

---
+++ src/testarea.lua
@@ -0,0 +1,16 @@
+squarePressed = true
+c = 0
+
+speed = 1
+while c ~= 105 do
+	--if speed ~= 100 then
+	--	speed = speed + 1
+	--end
+print(speed)
+
+
+	if speed ~= 1 then
+		speed = speed - 1
+	end
+	c = c + 1
+	end
